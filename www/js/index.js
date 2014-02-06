dc9b1820bbcaaa541c79f4b85708048a6bcc5ef3
Marco Rivadeneyra <marcorivm@gmail.com>
2014-02-06 17:42:50 -0600

    Basic todo app
    Signed-off-by: Marco Rivadeneyra <marcorivm@gmail.com>

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,109 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var TODO = function($form, $todo, $done) {
+    var tasks = [];
+    var TASK =  function(attributes) {
+
+        // Define TASK obj
+        var obj = {
+            attributes: {
+                'id' : attributes.id,
+                'name': attributes.name,
+                'status': ((attributes.done)? 'checked="checked"' : 'uncheked')
+            },
+            template: '<td>' +
+                            '<input type="checkbox" class="check-todo" id="todo_{{ID}}" {{STATUS}}>' +
+                        '</td>' +
+                        '<td><label for="todo_{{ID}}">{{NAME}}</label></td>' +
+                        '<td>' +
+                            '<div class="btn-group">' +
+                                '<button type="button" class="btn btn-info edit-todo"><span class="glyphicon glyphicon-edit"></span></button>' +
+                                '<button type="button" class="btn btn-danger delete-todo"><span class="glyphicon glyphicon-trash"></span></button>' +
+                            '</div>' +
+                        '</td>',
+            render: function() {
+                var skeleton = this.template;
+                skeleton = skeleton.replace(/{{ID}}/g, this.attributes.id);
+                skeleton = skeleton.replace(/{{NAME}}/g, this.attributes.name);
+                skeleton = skeleton.replace(/{{STATUS}}/g, this.attributes.status);
+                this.$el.html(skeleton);
+                return this;
+            },
+            update: function() {
+                this.render();
+                this.onUpdate(this);
+                this.bindEvents();
+                return this;
+            },
+            bindEvents: function() {
+                var obj = this;
+                obj.$el.on('click', '.delete-todo', function() {
+                    obj.$el.remove();
+                    obj.onDelete(obj);
+                });
+                obj.$el.on('click', '.edit-todo', function() {
+                    obj.attributes.name = prompt("Nuevo nombre");
+                    obj.update();
+                });
+                obj.$el.on('click', '.check-todo', function() {
+                    obj.attributes.status = (this.checked)? 'checked="checked"' : 'uncheked';
+                    obj.update();
+                });
+            },
+            isDone: function() {
+                return (this.attributes.status !== "uncheked");
+            },
+            onDelete: attributes.onDelete,
+            onUpdate: attributes.onUpdate,
+            $el: $('<tr>')
+        };
+
+        return obj.update();
+    };
+
+    // Define TODO obj
+    var obj = {
+        addTask: function (name, done) {
+            var new_task = TASK({
+                'name': name,
+                'done': done,
+                'id': tasks.length,
+                'onDelete': this.removeTask,
+                'onUpdate': this.updateTaskStatus
+            });
+            tasks.push(new_task);
+            if(done) {
+                $done.append(new_task.$el);
+            } else {
+                $todo.append(new_task.$el);
+            }
+        },
+        removeTask: function(task) {
+            tasks[task.attributes.id] = undefined;
+        },
+        updateTaskStatus: function(task) {
+            if(task.isDone() && $todo.find(task.$el)) {
+                task.$el.remove();
+                $done.append(task.$el);
+            } else if(!task.isDone() && $done.find(task.$el)) {
+                task.$el.remove();
+                $todo.append(task.$el);
+            }
+        },
+        'tasks': tasks
+    };
+
+    $form.on('submit', function () {
+        var $input = $form.find('input[name="todo-name"]');
+        obj.addTask($input.val());
+        $input.val('');
+        return false;
+    });
+
+    return obj;
+};
+
 var app = {
     // Application Constructor
     initialize: function() {
